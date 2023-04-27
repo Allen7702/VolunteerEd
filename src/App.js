b3a21d105c2b6bdb9a27f968e0df5713cb4435d3
johnson419 <johnsonfredrick577@.com>
2023-04-27 03:11:17 +0300

    added registration and improved UI

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Pricing from "./components/pricing/Pricing"
 import Contact from "./components/contact/Contact"
 import Footer from "./components/common/footer/Footer"
 import Home from "./components/home/Home"
+import Login from "./components/login_register/Login"
+import Register from "./components/login_register/Register"
 function App() {
   return (
     <>
@@ -19,6 +21,8 @@ function App() {
           <Route exact path='/pricing' component={Pricing} />
        
           <Route exact path='/contact' component={Contact} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
         </Switch>
         <Footer />
       </Router>
---
+++ src/components/login_register/Register.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./login_register.css";
+import { useLocation } from "react-router-dom";
+
+export const Register = () => {
+  const location = useLocation();
+
+  const [email, setEmail] = useState("");
+  const [password, setPass] = useState("");
+
+  return (
+    <>
+      <section className="toppy">
+        <h2>Home / {location.pathname.split("/")[1]}</h2>
+        <h1>Register</h1>
+      </section>
+      <div className="margin"></div>
+
+      <div className="apple">
+        <div className="auth-form-container">
+          <h2>Register</h2>
+          <form className="register-form">
+            <label htmlFor="email">Email</label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="Email"
+              id="email"
+              name="email"
+            />
+            <label htmlFor="password">password</label>
+            <input
+              value={password}
+              onChange={(e) => setPass(e.target.value)}
+              type="password"
+              placeholder="Password"
+              id="password"
+              name="password"
+            />
+            <button type="submit">Sign up</button>
+          </form>
+          <a href="/login">Already have an account? Login here.</a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Register;
